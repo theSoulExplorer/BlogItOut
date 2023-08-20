a8c417a732761cb35d31b7b5b98fa3e4ca9b3739
Aman Sharma <aman10201sharma@gmail.com>
2023-08-20 20:43:16 +0530

    #2

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.set("views",path.join(__dirname, './views'));
 app.set("view engine","ejs");
-app.use(express.static("src/public"));
+// app.use(express.static("src/public"));
+app.use(express.static(__dirname + '/public'));
 app.use("/user",userRouter);
 app.use("/blog",blogRouter);
 
